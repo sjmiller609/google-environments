89387f4cdf207193c78a2f52e127fe08bfc19353
Steven Miller <sjmiller609@gmail.com>
2019-07-24 18:51:32 +0000

    try deploying using pre-created TLS cert

--- stage/cloud/backend.tf
+++ stage/cloud/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.12"
   backend "gcs" {
-    bucket  = "astronomer-staging-terraform-state"
-    prefix  = "staging/terraform.tfstate"
+    bucket = "astronomer-staging-terraform-state"
+    prefix = "staging/terraform.tfstate"
   }
 }
--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -5,10 +5,28 @@ variable "kubeconfig_path" {
   type    = string
 }
 
+data "google_storage_object_signed_url" "fullchain" {
+  bucket = "${var.deployment_id}-astronomer-certificate"
+  path   = "fullchain.pem"
+}
+
+data "google_storage_object_signed_url" "privkey" {
+  bucket = "${var.deployment_id}-astronomer-certificate"
+  path   = "privkey.pem"
+}
+
+data "http" "fullchain" {
+  url = data.google_storage_object_signed_url.fullchain.signed_url
+}
+
+data "http" "privkey" {
+  url = data.google_storage_object_signed_url.privkey.signed_url
+}
+
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.196"
+  version = "0.1.198"
 
   deployment_id    = var.deployment_id
   dns_managed_zone = "staging-zone"
@@ -17,4 +35,7 @@ module "astronomer_cloud" {
   management_api   = "public"
   enable_gvisor    = false
   kubeconfig_path  = var.kubeconfig_path
+
+  tls_cert = data.http.fullchain.body
+  tls_key  = data.http.privkey.body
 }
