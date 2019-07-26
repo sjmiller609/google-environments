df9f332e25f28d49886c2381a48798cd625f1903
Steven Miller <sjmiller609@gmail.com>
2019-07-26 20:04:29 +0000

    add secrets to prod

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -7,30 +7,10 @@ variable "kubeconfig_path" {
   type    = string
 }
 
-data "google_storage_object_signed_url" "fullchain" {
-  bucket = "${var.deployment_id}-astronomer-certificate"
-  path   = "fullchain.pem"
-}
-
-data "google_storage_object_signed_url" "privkey" {
-  bucket = "${var.deployment_id}-astronomer-certificate"
-  path   = "privkey.pem"
-}
-
-# for this to work, the content type in the metadata on the
-# bucket objects must be "text/plain; charset=utf-8"
-data "http" "fullchain" {
-  url = data.google_storage_object_signed_url.fullchain.signed_url
-}
-
-data "http" "privkey" {
-  url = data.google_storage_object_signed_url.privkey.signed_url
-}
-
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.208"
+  version = "0.1.226"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
@@ -42,6 +22,8 @@ module "astronomer_cloud" {
   lets_encrypt           = false
   base_domain            = var.base_domain
 
-  tls_cert = data.http.fullchain.body
-  tls_key  = data.http.privkey.body
+  tls_cert          = data.http.fullchain.body
+  tls_key           = data.http.privkey.body
+  stripe_secret_key = data.http.stripe_secret_key.body
+  stripe_pk         = data.http.stripe_pk.body
 }
