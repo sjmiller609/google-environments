fe65e887cab06fb92fd1ba0eddc4fc86bb39b749
Drone CI <steven@astronomer.io>
2019-09-10 10:34:14 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.281: Bump to 0.10.1-alpha.3

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.279"
+  version = "0.1.281"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
