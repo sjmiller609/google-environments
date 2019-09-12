e5ba8823d36fc097937019b9c43fc86483df6feb
Drone CI <steven@astronomer.io>
2019-09-12 13:59:42 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.287: istio 1.3.0

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.285"
+  version = "0.1.287"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
