54598211005d4b8cd03e4481f8a19ee5b2c1c57f
Drone CI <steven@astronomer.io>
2019-09-20 15:03:33 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.368: [astronomer/issues#379] Restrict pods creation on Platform Node Pool (#33)
    * [astronomer/issues#379] Restrict pods creation on Platform Node Pool
    
    * Update main.tf

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.366"
+  version = "0.1.368"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
