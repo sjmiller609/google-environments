e85107e3d893d05edc255dbd31729b71b406b211
Drone CI <steven@astronomer.io>
2019-09-23 17:23:21 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.387: Deploy tiller to platform node pool

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.384"
+  version = "0.1.387"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
