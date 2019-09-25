3444661ec36ca89ded507aef0983b895e116a99d
Drone CI <steven@astronomer.io>
2019-09-25 15:54:00 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.401: Switch from resources to limits on istio sidecars

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.399"
+  version = "0.1.401"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
