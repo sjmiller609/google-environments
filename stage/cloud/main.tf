7f5ef153223d6d2bf9fb079c20f0705ea4c0bc6d
Drone CI <steven@astronomer.io>
2019-09-26 00:11:15 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.407: Remove runtimeClass and increase pgbouncer pool size

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.405"
+  version = "0.1.407"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
