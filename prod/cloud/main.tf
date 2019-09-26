5814c9414195a938d0e0337904b7f831a3a2a727
Drone CI <steven@astronomer.io>
2019-09-26 13:58:36 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.411: enable kiali

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.409"
+  version = "0.1.411"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
