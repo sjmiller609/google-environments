dd62ac9b6fc9c0ee70afe93f0709cc4692e31438
Drone CI <steven@astronomer.io>
2019-09-30 20:05:28 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.435: Drone CI: Update GCP infra module to 1.0.143

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.433"
+  version = "0.1.435"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
