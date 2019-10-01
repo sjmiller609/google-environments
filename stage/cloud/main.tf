7fee459f61a2424a64fd1e9e3fed845d37bdc0cf
Drone CI <steven@astronomer.io>
2019-10-01 14:57:38 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.453: Optionally enable Kubecost (#38)
    * Allow max workers to be configurable
    
    * Optionally add Kubecost

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.449"
+  version = "0.1.453"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
